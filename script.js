5e729fea7b67e500dd4fae873fe58fca69b9e724
Vasilisa <vasilisavygovskaya1109@gmail.com>
2024-07-15 11:41:28 +0300

    -15-07/1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const blocks = document.querySelector('.blocks');
 const wordsEng = document.querySelector('.words_eng');
 const phrasesEng = document.querySelector('.phrases_eng');
 const tensesEng = document.querySelector('.tenses_eng'); 
+const checkEng = document.querySelector('.check_eng'); 
 
 blocks.addEventListener('click', (event) => {
     const clickedBlock = event.target;
@@ -22,13 +23,26 @@ blocks.addEventListener('click', (event) => {
         console.log("tenses");
         tensesEng.scrollIntoView({ behavior: 'smooth' }); 
     }
+    else if (targetSection === 'check') 
+      { 
+          console.log("check");
+          checkEng.scrollIntoView({ behavior: 'smooth' }); 
+      }
 })
 
+const checkButton = document.getElementById('checkButton');
+const checkCards = document.querySelectorAll('.check_card');
 
-const cards = document.querySelectorAll('.card');
+checkButton.addEventListener('click', () => {
+  checkCards.forEach(card => {
+    const engWord = card.querySelector('.eng_word').textContent;
+    const userAnswer = card.querySelector('.user_answer').value;
+    const correctAnswer = card.parentElement.dataset.text2;
 
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    card.textContent = card.dataset.text2; // Извлекаем значение из `data-text2`
+    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
+      card.classList.add('correct');
+    } else {
+      card.classList.add('incorrect');
+    }
   });
 });
